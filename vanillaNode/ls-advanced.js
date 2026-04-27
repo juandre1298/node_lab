fb12e832647e2614a41b0dde79e284640003c70e
juan <juan>
2026-04-27 15:35:14 -0500

    Resolved

--- vanillaNode/ls-advanced.js
+++ vanillaNode/ls-advanced.js
@@ -1,17 +1,37 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
+import path from "node:path";
 // fs is for file system. And readdir read the directory. 
 // this is the async callback way
 
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder, (err, files)=>{
-    if(err){
-        console.error('Error al leer el directorio: ', err)
-        return;
-    }
-    files.forEach( file => {
-        console.log( file)
-    })
-})
 
+const ls = async (folder) => {
+    let files
+    try{
+        files = await fs.readdir(folder)
+    }catch(err){
+        console.error('Error when reaading the directory: ', err)
+        process.exit(1)
+    }
+    const filesPromises = files.map( async file => {
+        const filePath = path.join(folder,file)
+        let stats
+        try{
+            stats = await fs.stat(filePath)
+            return {
+                name: file,
+                size_mb: stats.size/1024,
+                type: stats.isDirectory() ? 'dir' : 'file',
+                lastModificationTime: stats.mtime
+            }
+        }catch(err){
+            console.error(`can't get ${filePath} stats: ${err}`)
+        }      
+        return;
+    })
+    Promise.all(filesPromises).then(ans => console.log(ans))
+}
+
+ls(folder)
 
